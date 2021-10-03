aef6595910f5c4911c33ac1c270eb5ef8a42ec05
Nilton Oliveira <nilton.oliveira0305@gmail.com>
2021-10-03 04:16:25 -0300

    teste dos scrips

---
+++ exemplo.js
@@ -0,0 +1,8 @@
+const array = [1, 2, 3, 5]
+const novoArray = array.map(
+    (item) => {
+        return item + i
+    }
+)
+
+console.log(novoArray)
--- script/js/indices.js
+++ script/js/indices.js
@@ -2,47 +2,43 @@ const db = () => {
 
     const indices = [
         {
-            title: "Estrutura básica HTML",
-            article: "lorem ansiufsidufhsiuhdfiushifushdifuhsdiufh"
+            title: "Manipular Array - Array.prototype.map()",
+            article: `<p>O metodo map() cria um novo array preenchedo-o
+            com os resultados da chamada de uma função fornecida em cada elemento do array de 
+            chamada. Exemplificando com código:</p> <br>
+            <img src="../../img/map.png" alt="imagem">
+            <p>Sua resposta será um array = [ 2, 3, 4, 6 ].</p><br>
+            <p>O map passa por cada item do array, por isso ele recebe uma função com um parametro, 
+            que é o item do array que ele vai pecorrer, e retornará o item com a alteração que você 
+            quiser. No caso do exemplo, cara item do array foi soma mais 1.</p>
+            `
+        
         },
-        {
-            title: "Tag P",
-            article: "lorem ansiufsidufhsiuhdfiushifushdifuhsdiufh"
-        },
-        {
-            title: "Tag H, h1 à h6",
-            article: "lorem ansiufsidufhsiuhdfiushifushdifuhsdiufh"
-        },
-        {
-            title: "Tag a - Âncora",
-            article: "lorem ansiufsidufhsiuhdfiushifushdifuhsdiufh"
-        },
-        {
-            title: "Tag br",
-            article: "ajsidudhfuxncvncjvjcvj"
 
-        },
-        {
-            title: "Tag address - Informação de contato",
-            article: "lorem ansiufsidufhsiuhdfiushifushdifuhsdiufh"
-        },
-        {
-            title: "Tags area e map - mapeando links",
-            article: "lorem ansiufsidufhsiuhdfiushifushdifuhsdiufh"
-        },
     ]
 
-    
-    
+
     const showIndices = () => {
         let li = ""
         let divArticles = ""
-        for(let indice of indices){
-            li += `<li><a href = "#${indice.title}" >${indice.title}</a></li>`
-            divArticles += `<div id = "${indice.title}"><h1>${indice.title}</h1><p>${indice.article}</p></div>`
+        for (let indice of indices) {
+            li += `
+            <li>
+                <a href = "#${indice.title}">${indice.title}</a>
+            </li>
+            `
+
+            divArticles += `
+            <div id = "${indice.title}">
+                <h1>${indice.title}</h1><br>
+                <article>${indice.article}</article>
+            </div>
+            `
         }
         document.querySelector(".indicesUl").innerHTML = li
         document.querySelector('.articles').innerHTML = divArticles
+
+        
     }
 
     showIndices()
