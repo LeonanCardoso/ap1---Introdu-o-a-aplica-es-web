9df1ad819241719cc12f0dfe2bb48c6c0defb98a
Nilton Oliveira <nilton.oliveira0305@gmail.com>
2021-10-03 14:04:57 -0300

    adicionei um artigo

--- database/javascript.js
+++ database/javascript.js
@@ -1,16 +1,24 @@
 export const database = [
+  
+  
   {
       title: "Manipular Array - Array.prototype.map()",
       article: `
-      <p>O metodo map() cria um novo array preenchedo-o
+      <p>
+      
+      O metodo map() cria um novo array preenchedo-o
       com os resultados da chamada de uma função fornecida em cada elemento do array de 
-      chamada. Exemplificando com código:</p><br>
+      chamada. Exemplificando com código:
+      
+      </p><br>
 
-      <img src="../../img/map.png" alt="imagem">
+      <img src="../../img/javascript/map.png" alt="imagem"><br><br>
 
       <p>Sua resposta será um array = [ 2, 3, 4, 6 ].</p><br>
       
-      <p>O map passa por cada item do array, por isso ele recebe uma função com um parametro, que é o item do array que ele vai pecorrer, e retornará o item com a alteração que você quiser. No caso do exemplo, cara item do array foi soma mais 1.
+      <p>
+      
+      O map passa por cada item do array, por isso ele recebe uma função com um parametro, que é o item do array que ele vai pecorrer, e retornará o item com a alteração que você quiser. No caso do exemplo, cara item do array foi soma mais 1.
       
       </p>
       `
---
+++ e.js
@@ -0,0 +1,4 @@
+const array = [1, 2, 3, 4, 5]
+const novoArray = array.map(item => item + 1)
+
+console.log(novoArray)
